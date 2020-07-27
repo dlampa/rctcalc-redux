93ee10fb22e996ded8a4e22fef18f71f271ad789
altryx <lampa@altx.dev>
2020-07-27 17:58:31 -0600

    Added buttons, text input and basic number entry is functional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import './App.css';
+import CalcButton from './CalcButton';
+
+let numChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let opChars = ["+", "-", "*", "/", "="];
+
 
 class App extends React.Component 
 {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      calcInput: ""
+    };
+    
+  }
+
+  onInput = (event) => {
+    this.setState({calcInput: event.target.innerText})
+  }
+
+  updateState = (key, value) => {
+    this.setState({ [key]: value });
+  }
+
   render()
   {
-    return (null);
+    return (
+      <>
+      <div id="calcDisplay">
+        <input type="text" id="calcOutput" value={this.state.calcInput} onChange={(event) => this.updateState("calcInput", event.target.value)} />
+      </div>
+      <div id="calcControls">
+          {numChars.map(numChar => <CalcButton renderVal={numChar} className="btnNumbers" onClick={this.onInput}/>)}
+        {opChars.map(opChar => <CalcButton renderVal={opChar} className="btnOpers" /> )}
+      </div>
+      </>
+    );
   }
 
 }
---
+++ src/CalcButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class CalcButton extends React.Component
+{
+    onButtonClicked = (event) => {
+        this.props.onClick(event);
+    }
+
+    render()
+    {
+        return (
+            <button id={"calcButton" + this.props.renderVal.toString()}
+                className={this.props.className}
+                onClick={(event) => this.onButtonClicked(event)}>
+                {this.props.renderVal}
+            </button>
+        );
+    }
+}
+
+export default CalcButton;
